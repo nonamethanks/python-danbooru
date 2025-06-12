a2a1250edcde8896c418c85c2191bd14090781a1
nonamethanks <hellafrickingepic@gmail.com>
2025-06-12 13:57:56 +0200

    Update post report repr

--- danbooru/models/post_report.py
+++ danbooru/models/post_report.py
@@ -10,3 +10,7 @@ from danbooru.model import DanbooruModel
 class DanbooruPostReport(DanbooruModel):
     date: datetime.datetime = None
     posts: int
+
+    def __repr__(self) -> str:
+        props = " ".join(f"{k}={v}" for k, v in self.model_dump(exclude_none=True).items())
+        return f"{type(self).__name__}<{props}>"
