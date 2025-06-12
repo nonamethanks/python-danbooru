614ee9051bd97e9980da9672d265b9838f976f20
nonamethanks <hellafrickingepic@gmail.com>
2025-06-12 16:36:40 +0200

    Add user level serialization

--- danbooru/user_level.py
+++ danbooru/user_level.py
@@ -2,7 +2,7 @@
 
 from __future__ import annotations
 
-from pydantic import model_validator
+from pydantic import model_serializer, model_validator
 
 from danbooru.utils import BaseModel
 
@@ -27,32 +27,53 @@ class UserLevel(BaseModel):
 
     @model_validator(mode="before")
     @classmethod
-    def validate_model(cls, value: int | str | UserLevel) -> dict:
+    def validate_model(cls, value: int | str | dict | UserLevel) -> dict:
         """Validate that the level is valid."""
         if isinstance(value, str):
             name = value.upper()
-            try:
-                number = LEVEL_MAP[name]
-            except KeyError as e:
-                msg = f"No level with name {name}."
-                raise ValueError(e) from e
+            number = cls.number_from_name(name)
 
         elif isinstance(value, int):
             number = value
-            try:
-                number_index = list(LEVEL_MAP.values()).index(number)
-                name = list(LEVEL_MAP.keys())[number_index]
-            except (IndexError, ValueError) as e:
-                msg = f"No level with number {number}."
-                raise ValueError(msg) from e
+            name = cls.name_from_number(number)
+
         elif isinstance(value, UserLevel):
             name = value.name
             number = value.number
+
         else:
-            e = f"{type(value)}: not an acceptable value."
+            e = f"{value} ({type(value)}): not an acceptable value."
             raise TypeError(e)
 
         return {"number": number, "name": name}
+
+    @model_serializer
+    def serializer(self) -> dict[str, int]:
+        """Serialize the level."""
+        return {"level": self.number}
+
+    @staticmethod
+    def name_from_number(number: int) -> str:
+        """Return the level number from a name."""
+        try:
+            number_index = list(LEVEL_MAP.values()).index(number)
+            name = list(LEVEL_MAP.keys())[number_index]
+        except (IndexError, ValueError) as e:
+            msg = f"No level with number {number}."
+            raise ValueError(msg) from e
+        else:
+            return name
+
+    @staticmethod
+    def number_from_name(name: str) -> int:
+        """Return the level name from a number."""
+        try:
+            number = LEVEL_MAP[name]
+        except KeyError as e:
+            msg = f"No level with name {name}."
+            raise ValueError(msg) from e
+        else:
+            return number
 
     def __lt__(self, level: int | UserLevel | str) -> bool:
         return self.number < UserLevel(level).number
