0ac612620e454c8a4047334dfe183449d3de3836
nonamethanks <hellafrickingepic@gmail.com>
2025-06-22 15:56:29 +0200

    Add way to extract linked tags from wiki page

--- danbooru/models/wiki_page.py
+++ danbooru/models/wiki_page.py
@@ -1,7 +1,16 @@
 """Model definition for /wiki_pages."""
+import re
 
 from danbooru.model import DanbooruModel
 
 
 class DanbooruWikiPage(DanbooruModel):
-    ...
+    body: str
+
+    @property
+    def linked_tags(self) -> list[str]:
+        """Return a list of linked tags in a wiki's body."""
+        pattern = re.compile(r"\[\[([^|\[\]]+)(?:\|(.*))?\]\]")
+
+        tags = [t[0].strip() for t in pattern.findall(self.body)]
+        return [t.replace(" ", "_").lower() for t in tags]
