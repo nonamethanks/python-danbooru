107f42c1689b2b20ddc67d9778119d63fd165911
nonamethanks <hellafrickingepic@gmail.com>
2025-06-23 15:32:50 +0200

    Fix some implications having null approver ID

--- danbooru/models/tag_implication.py
+++ danbooru/models/tag_implication.py
@@ -9,6 +9,6 @@ class DanbooruTagImplication(DanbooruModel, _DanbooruModelWithId):
     antecedent_name: str
     consequent_name: str
     status: str
-    approver_id: int
+    approver_id: int | None
     forum_post_id: int | None
     forum_topic_id: int | None
