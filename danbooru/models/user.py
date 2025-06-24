6d8bbd2de1d2302a065250be5d19ee4bcf8bf30a
nonamethanks <hellafrickingepic@gmail.com>
2025-06-24 18:16:25 +0200

    Fix users not having updated_at

--- danbooru/models/user.py
+++ danbooru/models/user.py
@@ -1,8 +1,11 @@
 """Model definition for /users."""
 
+import datetime
+
 from danbooru.model import DanbooruInstancedModel
 from danbooru.user_level import UserLevel
 
 
 class DanbooruUser(DanbooruInstancedModel):
     level: UserLevel
+    updated_at: datetime.datetime | None = None
