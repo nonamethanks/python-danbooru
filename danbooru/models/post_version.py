7ff172cff715c1a94c833b2eaf86faacaa16986d
nonamethanks <hellafrickingepic@gmail.com>
2025-12-09 18:22:33 +0100

    Add more props to post versions

--- danbooru/models/post_version.py
+++ danbooru/models/post_version.py
@@ -1,8 +1,22 @@
 """Model definition for /post_versions."""
 
 
+from pydantic import field_validator
+
 from danbooru.model import DanbooruInstancedModel
 
 
 class DanbooruPostVersion(DanbooruInstancedModel):
     created_at: None = None
+
+    added_tags: list[str]
+    removed_tags: list[str]
+    obsolete_added_tags: list[str]
+    obsolete_removed_tags: list[str]
+
+    @field_validator("obsolete_added_tags", "obsolete_removed_tags", mode="before")
+    def split_string(cls, v: str) -> str:
+        """Turn the tag strings into tag arrays."""
+        if isinstance(v, str):
+            return v.split()
+        return v
