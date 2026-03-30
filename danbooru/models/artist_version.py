c5a5ce1e29eb2e23f3da7f47ba531ca6bf5acf51
nonamethanks <hellafrickingepic@gmail.com>
2026-03-30 16:28:14 +0200

    Add version pages

---
+++ danbooru/models/artist_version.py
@@ -0,0 +1,8 @@
+"""Model definition for /artist_versions."""
+
+
+from danbooru.model import DanbooruInstancedModel
+
+
+class DanbooruArtistVersion(DanbooruInstancedModel):
+    ...
---
+++ danbooru/models/wiki_page_version.py
@@ -0,0 +1,8 @@
+"""Model definition for /wiki_page_versions."""
+
+
+from danbooru.model import DanbooruInstancedModel
+
+
+class DanbooruWikiPageVersion(DanbooruInstancedModel):
+    ...
